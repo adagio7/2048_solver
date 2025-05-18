abc46604cd0e26ffaeb41f39e30ebbf1f42b36fc
adagio7 <haojiepe@gmail.com>
2025-05-18 20:17:52 +0100

    Add random solver

---
+++ src/solvers/__init__.py
@@ -0,0 +1,5 @@
+# Needed to register the solvers correctly
+from .registry import SolverRegistry
+
+from .human import HumanSolver
+from .random import RandomSolver
---
+++ src/solvers/random.py
@@ -0,0 +1,26 @@
+from random import choice
+from typing import List
+
+from .solver import Solver
+from .registry import SolverRegistry
+from ..models import Moves
+
+@SolverRegistry.register
+class RandomSolver(Solver):
+    """
+    A solver that randomly selects a move from the available options.
+    """
+    name = "Random"
+
+    def __init__(self):
+        super().__init__()
+
+    def get_move(self, game_state: List[int]) -> Moves:
+        """
+        Randomly selects a move from the available options.
+        
+        :param game_state: The current state of the game.
+        :return: A randomly selected move.
+        """
+        # Cast enum to sequence to allow random choice
+        return choice(list(Moves))
