f9e1922d7cd9f854aabffa6516c99c0bca6a0b6e
adagio7 <haojiepe@gmail.com>
2025-05-18 20:18:19 +0100

    Update typing for solver

--- src/solvers/solver.py
+++ src/solvers/solver.py
@@ -1,9 +1,12 @@
 from abc import ABC, abstractmethod
+from typing import List
+
+from ..models import Moves
 
 class Solver(ABC):
     name: str = "Solver"
 
-    def get_move(self, game_state):
+    def get_move(self, game_state: List[int]) -> Moves:
         """
         Get the next move for the given game state.
         
